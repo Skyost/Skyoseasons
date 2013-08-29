b9aca4b0f9d30bd2f7ee06e5e8ca489d2346ddcd
Skyost <skyost@mail.com>
2013-08-30 00:20:48 +0200

    De-Mavenized

--- src/com/skyost/seasons/SeasonsPlugin.java
+++ src/com/skyost/seasons/SeasonsPlugin.java
@@ -9,11 +9,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.mcstats.Metrics;
-import org.mcstats.Metrics.Graph;
 
 import com.skyost.seasons.api.Season;
 import com.skyost.seasons.api.Skyoseasons;
+import com.skyost.seasons.util.Metrics;
+import com.skyost.seasons.util.Metrics.Graph;
 import com.skyost.seasons.util.PluginConfig;
 
 public class SeasonsPlugin extends JavaPlugin implements Listener {
