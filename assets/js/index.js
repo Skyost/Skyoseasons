71b185648e903538db27e96ea05de20fee492075
Skyost <skyost@mail.com>
2015-08-16 15:52:55 +0200

    Added links and fixed responsive with a false background.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,14 @@ var seasons = ['spring', 'summer', 'autumn', 'winter'];
 var season = seasons[Math.floor(Math.random() * seasons.length)];
 
 $(document).ready(function() {
+	$('#background img').attr('src', 'assets/img/' + season + '/background.png');
 	$('#banner img').attr('src', 'assets/img/' + season + '/banner.png');
 	$('#season img').attr('src', 'assets/img/' + season + '/season.png');
-	var body = $('body');
+	/*var body = $('body');
 	body.css('background', 'url(\'assets/img/' + season + '/background.png\') no-repeat center center fixed');
 	body.css('-webkit-background-size', 'cover');
 	body.css('-moz-background-size', 'cover');
 	body.css('-o-background-size', 'cover');
-	body.css('background-size', 'cover');
+	body.css('background-size', 'cover');*/
 	$('img').tooltip();
 });
